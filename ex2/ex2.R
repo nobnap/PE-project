1eefe2eb6b36717830269b87962ef4bd72eb459c
Maria Rosa <100365639+nobnap@users.noreply.github.com>
2023-06-15 18:54:16 +0100

    Removed irrelevant code from exercise 2

--- ex2/ex2.R
+++ ex2/ex2.R
@@ -5,10 +5,9 @@ dados <- subset(dados, País!='África do Sul')
 
 cuidados <- subset(dados, Ocupação=='Cuidados pessoais' & Sexo=='Homens')
 cuidados <- cuidados['Tempo']
-cuidados
+
 trabalho <- subset(dados, Ocupação=='Trabalho remunerado ou estudo' & Sexo=='Homens')
 trabalho <- trabalho['Tempo']
-trabalho
 
 par(mfrow = c(1, 2))
 boxplot(cuidados, col = "red", main = "Cuidados Pessoais")
