b8c594d95cbe2d794205ac057157f3af719616a3
Maria Rosa <100365639+nobnap@users.noreply.github.com>
2023-06-15 18:17:16 +0100

    Solved exercise 3

--- ex3/ex3.R
+++ ex3/ex3.R
@@ -1,11 +1,13 @@
 library(ggplot2)
 dados <- read.table("GENDER_EMP_19032023152556091.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-colnames(dados)
 dados <- subset(dados[c(2, 3, 5, 8, 10, 17)], Country=='Austria' 
                 & IND=='EMP1' & Time=='2000' & SEX!='ALL_PERSONS'
                 & Age.Group!='Total')
-dados
-ggplot(dados, aes(x=Age.Group, y=Value, group=Age.Group, col=as.factor(Age.Group)) )
 
+ggplot(dados, aes(x = Age.Group, y = Value, fill = SEX)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Age Group", y = "Value", fill = "Sex") +
+  ggtitle("Comparison of Women and Men in Different Age Ranges") +
+  theme_minimal()
 
 
