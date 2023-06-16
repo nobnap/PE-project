e9bcc8e40aa91cd215b1131c2bea7e0720e900b1
Maria Rosa <100365639+nobnap@users.noreply.github.com>
2023-06-16 15:25:35 +0100

    Solved ex7.R

---
+++ ex7/ex7.R
@@ -0,0 +1,11 @@
+set.seed(1840)
+
+population <- replicate(2542, rnorm(10, mean = 0, sd = 1))
+population <- t(population)
+
+quadrados <- apply(population, 1, function(row) sum(row^2))
+
+quantil <- quantile(quadrados, probs=0.22, type=2)
+teo <- qchisq(0.22, 10)
+
+teo-quantil
