b57ffe5393e1bbcb8494ca06570ca67edb387aa1
Maria Rosa <100365639+nobnap@users.noreply.github.com>
2023-06-17 15:44:27 +0100

    Fixed exercise 2

--- ex2/ex2.R
+++ ex2/ex2.R
@@ -1,15 +1,19 @@
+library(ggplot2)
 dados <- read.csv("TIME_USE_24092022.csv", sep=",")
 
 dados <- subset(dados, País!='África do Sul')
 
-
 cuidados <- subset(dados, Ocupação=='Cuidados pessoais' & Sexo=='Homens')
-cuidados <- cuidados['Tempo']
+cuidados <- cuidados[c('Tempo', 'Ocupação')]
 
 trabalho <- subset(dados, Ocupação=='Trabalho remunerado ou estudo' & Sexo=='Homens')
-trabalho <- trabalho['Tempo']
+trabalho <- trabalho[c('Tempo', 'Ocupação')]
 
-par(mfrow = c(1, 2))
-boxplot(cuidados, col = "red", main = "Cuidados Pessoais")
-boxplot(trabalho, col = "blue", main = "Trabalho Remunerado\n ou Estudo")
+data <- rbind(cuidados, trabalho)
 
+ggplot(data, aes(x = Ocupação, y = Tempo, fill=Ocupação)) +
+  geom_boxplot() +
+  labs(x = "Ocupação", y = "Tempo", title = "Comparação do tempo diário gasto por homens\n em diferentes ocupações") +
+  scale_color_manual(values = c("blue", "red"))+
+  theme_minimal()+
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5))
