9886da39e69280d4822c79519fa3cd22a0f45804
Maria Rosa <100365639+nobnap@users.noreply.github.com>
2023-06-17 15:24:21 +0100

    Added solved exercise 8

---
+++ ex8/ex8.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+set.seed(1441)
+
+probs <- 1:174 / (174 + 1)
+
+cauchy <- rcauchy(174, location = -1.4, scale = 2)
+cauchy <- sort.int(cauchy)
+
+quantil_c <- quantile(cauchy, probs=probs, type=2)
+
+quantil_n <- qnorm(probs, mean=-0.7, sd=2)
+
+df <- data.frame(
+  Values = c(cauchy),
+  Quantiles = c(quantil_c, quantil_n),
+  Population = rep(c("Cauchy", "Normal"), each = 174)
+)
+
+ggplot(df, aes(x = Values, y = Quantiles, color = Population)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, color = "green") +
+  labs(x = "Sorted Values", y = "Probability Quantiles", title = "Cauchy vs Normal Population") +
+  scale_color_manual(values = c("blue", "red")) +
+  theme_minimal() +
+  theme(legend.position = "bottom", plot.title = element_text(hjust = 0.5))
